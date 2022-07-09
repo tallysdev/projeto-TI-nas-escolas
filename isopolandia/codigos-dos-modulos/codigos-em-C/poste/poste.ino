94bcc1995ab4bb1f5cc1c4535e0ca452ea1ab5e6
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-09 15:09:06 -0300

    main: add codigo do poste

---
+++ isopolandia/codigos-dos-modulos/codigos-em-C/poste/poste.ino
@@ -0,0 +1,27 @@
+#define ldrPin A0
+#define ledPin1 6
+#define ledPin2 7
+
+void setup() {
+  pinMode(ldrPin, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println(analogRead(ldrPin));
+  
+  if (analogRead(ldrPin) > 1010) {
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+  }
+
+  else {
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    
+  }
+
+}
